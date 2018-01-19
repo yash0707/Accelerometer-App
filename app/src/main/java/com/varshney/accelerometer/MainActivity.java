996e27b7d3d7c3b805c36e7770e91cbf1a2b6a53
sherlock07 <yvarshney0707@gmail.com>
2018-01-19 23:56:05 +0530

    Accelerometer Code Added

--- app/src/main/java/com/varshney/accelerometer/MainActivity.java
+++ app/src/main/java/com/varshney/accelerometer/MainActivity.java
@@ -1,15 +1,47 @@
 package com.varshney.accelerometer;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    TextView tvX,tvY,tvZ;
+    SensorManager sm;
+    Sensor s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        s = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        sm.registerListener(this,s,SensorManager.SENSOR_DELAY_NORMAL);
+
+        tvX = (TextView) findViewById(R.id.tvX);
+        tvY = (TextView) findViewById(R.id.tvY);
+        tvZ = (TextView) findViewById(R.id.tvZ);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        tvX.setText("X:- "+ event.values[0]);
+        tvY.setText("Y:- "+ event.values[1]);
+        tvZ.setText("Z:- "+ event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
 }
